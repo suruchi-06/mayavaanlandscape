42c89e3129e3043ea2d4f1da84df1c33092373be
suruchi-06 <suruchi06.k@gmail.com>
2026-05-20 18:39:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
-    routers: {
-      client: {
-        entry: "src/routes/__root.tsx",
-      },
-    },
   },
 });
